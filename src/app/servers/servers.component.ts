a7c73e8bfcb3c4088d0f9785869c48b9276a9f56
Aszmel <mellipone@gmail.com>
2019-09-07 21:35:09 +0100

    added bootstrap, some css, my own component by hand and created by ng g c Component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  //below can use no Url to put template in use
+  // template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ["./servers.component.css"]
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
