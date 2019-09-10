614cf1404606cb23fcf1eae4067ff4e924c3095a
Aszmel <mellipone@gmail.com>
2019-09-10 18:08:16 +0100

    event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   templateUrl: "./server.component.html"
 })
 export class ServerComponent {
-  serverId: number = 11;
+  serverId: number = 10;
   serverStatus: string = "offline";
   getServerStatus() {
     return this.serverStatus;
